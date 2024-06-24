e3017c5de8f09d70c0f42925a7bba6890b5e04a2
Damandeep <official.damandeepsingh@gmail.com>
2024-06-24 13:17:14 +0530

    practice of general concepts

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 //  add two numbers in ts
-
 // function addTwoNumber(x:number,y:number):number{
 // return x+y;
 // }
 // console.log(addTwoNumber(3,4))
-
-
 //  inteface
-
-
 // interface Person{
 //     name:string
 //     age:number
 // }
-
 // class PersonImp implements Person{
 //     name:string;
 //     age:number;
-
 //     constructor(name:string,age:number){
 //         this.name=name;
 //         this.age=age
@@ -25,16 +18,10 @@
 // }
 // let person:Person=new PersonImp("Alice",30);
 // console.log(person.name,person.age);
-
-
-
 // type
-
 // let numbers:number []=[1,2,3];
 // console.log(numbers);
-
 // class
-
 // class Car{
 //     make:string;
 //     model:string;
@@ -46,15 +33,11 @@
 //         this.model=model;
 //     }
 // }
-
 // let tata=new Car('tata','2019');
 // tata.displayInfo();
-
-
 // function
-function firstElement<T>(arr:T[]):T{
+function firstElement(arr) {
     return arr[0];
-    
 }
-let arr:number []=[1,2,3]
+var arr = [1, 2, 3];
 console.log(firstElement(arr));
